2ef5781982b0ba517d4aab5addcf3a6488c22cf4
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-06-16 11:13:20 +0700

    cach su dung bien so voi JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,12 @@
 import "./style.css";
 
 const MyComponent = () => {
+  const myName = "John Doe";
+
   return (
     <>
-      <div>MyComponent</div>
+      <div>{myName}</div>
+      {console.log("This is a console log message")}
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
