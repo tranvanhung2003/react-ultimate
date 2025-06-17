8fbc2f3d0ec5a817c375fc2cdecfaf4e4ccd54bf
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-06-17 21:04:01 +0700

    tao header/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 import "./components/todo/todo.css";
 import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
@@ -29,17 +31,21 @@ const App = () => {
   };
 
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew addNewTodo={addNewTodo} />
-      {todoList.length > 0 ? (
-        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
-      ) : (
-        <div className="todo-image">
-          <img className="logo" src={reactLogo} alt="React Logo" />
-        </div>
-      )}
-    </div>
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew addNewTodo={addNewTodo} />
+        {todoList.length > 0 ? (
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+        ) : (
+          <div className="todo-image">
+            <img className="logo" src={reactLogo} alt="React Logo" />
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>React Vite @tranvanhung2003</p>
+    </div>
+  );
+};
+
+export default Footer;
