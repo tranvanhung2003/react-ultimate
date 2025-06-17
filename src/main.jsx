ad2947cd701a6c2e1f3d10c1b9a72ab1ad192bea
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-06-17 18:22:29 +0700

    tich hop router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact</div>,
+  },
+  {
+    path: "/services",
+    element: <div>Services</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
